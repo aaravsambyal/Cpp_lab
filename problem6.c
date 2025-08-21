006d0250efb15c658cb92ead863b8cedadd5ee45
aaravsambyal <iamaaravsambyal@gmail.com>
2025-08-21 11:42:48 +0530

    correction

--- problem6.c
+++ problem6.c
@@ -1,13 +1,15 @@
-#include<stdio.h>
-int main(){
-int i = 0;
-int arr[] = {4,6,7,2,9};
-int *ptr = arr;
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    int arr[] = {4, 6, 7, 2, 9};
+    int *ptr = arr;
 
-for(int i=0;i<5;i++){
+    for (int i = 0; i < 5; i++)
+    {
 
-    printf("%d\n", *ptr);
-    ptr++;
-
-}
+        printf("%d\n", *ptr);
+        ptr++;
+    }
+    return 0;
 }
