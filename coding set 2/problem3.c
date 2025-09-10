209d813e59a53e3efdae1c3df3d88c5cd0ca0d2b
aaravsambyal <iamaaravsambyal@gmail.com>
2025-09-10 16:27:13 +0530

    problem4

--- coding set 2/problem3.c
+++ coding set 2/problem3.c
@@ -1,28 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
     int n;
     int evensum = 0;
-    int oddsum = 0; 
+    int oddsum = 0;
     printf("No of elements in array: \n");
     scanf("%d", &n);
     int arr[n];
     printf("Enter elements: \n");
-    for (int i=0; i<n; i++){
-        scanf("%d", arr+i);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", arr + i);
+    }
+    int *ptr = arr;
+    for (int i = 0; i < n; i++)
+    {
+        if (*(ptr + i) % 2 == 0)
+        {
+            evensum += *(ptr + i);
         }
-        int *ptr = arr;
-for (int i=0; i<n; i++){
-    if (*(ptr+i) % 2 == 0){
-        evensum += *(ptr+i);
+        if (*(ptr + i) % 2 != 0)
+        {
+            oddsum += *(ptr + i);
+        }
     }
-    if (*(ptr+i) % 2 != 0){
-        oddsum += *(ptr+i);
-    }
+    printf("Sum of even elements: %d\n ", evensum);
+    printf("Sum of odd elements: %d", oddsum);
+
+    return 0;
 }
-printf("Sum of even elements: %d\n ", evensum);
-printf("Sum of odd elements: %d", oddsum);
-
-return 0;
-
-    }
