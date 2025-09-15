97ad2ce0e3e3ebc7a111f258ee85dbf09998abfd
aaravsambyal <iamaaravsambyal@gmail.com>
2025-09-15 17:29:30 +0530

    .

---
+++ coding set 2/problem8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[3][3][3];
+    printf("Enter the elements of array: \n");
+
+    int *ptr = &arr[0][0][0];
+    for (int i = 0; i < 27; i++)
+    {
+        scanf("%d", ptr + i);
+    }
+    printf("The elements of array are: \n");
+    for (int i = 0; i < 27; i++)
+    {
+        printf("%d", *(ptr + i));
+    }
+}
