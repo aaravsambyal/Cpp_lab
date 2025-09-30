d847ce05898bab7bd09659932e062953e1d05fd9
Aarav Sambyal <iamaaravsambyal@gmail.com>
2025-09-30 22:44:51 +0530

    .

--- coding set 4/problem6.cpp
+++ coding set 4/problem6.cpp
@@ -1,21 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Box
 {
     int length, width, height;
 
-    public:
+public:
     Box(int l = 0, int w = 0, int h = 0)
     {
         length = l;
         width = w;
         height = h;
     }
-    
+
     int volume()
     {
-        return length*width*height;
+        return length * width * height;
     }
 
     void compare(Box b)
@@ -25,25 +25,24 @@ class Box
 
         if (vol1 > vol2)
         {
-            cout<<"Passed Box has smaller volume";
+            cout << "Passed Box has smaller volume";
         }
         else if (vol1 == vol2)
         {
-            cout<<"Passed Box has same volume";
+            cout << "Passed Box has same volume";
         }
         else
         {
-            cout<<"Passed Box has larger volume";
+            cout << "Passed Box has larger volume";
         }
     }
-
 };
 
 int main()
 {
-    Box B1(2, 3, 6); //36
-    Box B2(6, 4, 8); //192
-    Box B3(2, 2, 4); //16
-    Box B4(2, 3, 6); //36
+    Box B1(2, 3, 6); // 36
+    Box B2(6, 4, 8); // 192
+    Box B3(2, 2, 4); // 16
+    Box B4(2, 3, 6); // 36
     B1.compare(B2);
 }
