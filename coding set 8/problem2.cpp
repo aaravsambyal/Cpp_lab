28c397d9928ab7368dfd806bc7c427ee90a7ced5
Aarav Sambyal <iamaaravsambyal@gmail.com>
2025-11-24 16:20:35 +0530

    .

---
+++ coding set 8/problem2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    string fileName;
+    cout << "Enter file name:";
+    getline(cin, fileName);
+
+    ifstream file(fileName);
+    if (!file)
+    {
+        cout << "File does not exist";
+    }
+
+    int wordCount = 0;
+    int charCount = 0;
+    int lineCount = 0;
+    string line;
+
+    while (getline(file, line))
+    {
+        lineCount++;
+
+        charCount += line.length();
+    }
+    file.clear();
+    file.seekg(0);
+    string word;
+    while (file >> word)
+    {
+        wordCount++;
+    }
+
+    file.close();
+
+    cout << "Lines: " << lineCount << '\n';
+    cout << "Characters: " << charCount << '\n';
+    cout << "Words: " << wordCount << '\n';
+}
